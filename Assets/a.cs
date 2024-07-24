4c9ac113e872de55ecca24eb17f5876b7de90841
WDEXcourse <wdkawasaki.online@gmail.com>
2024-07-24 21:38:15 +0900

    first commit

---
+++ Assets/a.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class a : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("up")){
+            transform.position+=transform.forward*0.1f;
+        }
+        if(Input.GetKey("right")){
+            transform.Rotate(0,10,0);
+        }
+        if(Input.GetKey("left")){
+            transform.Rotate(0,-10,0);
+        }
+    }
+}
