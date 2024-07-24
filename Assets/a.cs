610979d76e8bc1da6c4f6bef70e508b806c75d4c
WDEXcourse <wdkawasaki.online@gmail.com>
2024-07-24 21:53:35 +0900

    add_settings

--- Assets/a.cs
+++ Assets/a.cs
@@ -22,5 +22,8 @@ public class a : MonoBehaviour
         if(Input.GetKey("left")){
             transform.Rotate(0,-10,0);
         }
+        if(Input.GetKey("back")){
+            transform.position+=transform.backward*0.1f;
+        }
     }
 }
